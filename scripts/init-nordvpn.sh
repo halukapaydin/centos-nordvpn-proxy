c9c3bc7f940759a887f899765b0cfca2da6728b7
Haluk Apaydın <halukapaydin@gmail.com>
2019-07-30 15:02:44 +0300

    change

--- scripts/init-nordvpn.sh
+++ scripts/init-nordvpn.sh
@@ -2,19 +2,21 @@
 
 touch log.txt
 
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 >> log.txt
-sysctl -w net.ipv6.conf.default.disable_ipv6=1 >> log.txt
-sysctl -w net.ipv6.conf.tun0.disable_ipv6=1 >> log.txt
+#sysctl -w net.ipv6.conf.all.disable_ipv6=1 >> log.txt
+#sysctl -w net.ipv6.conf.default.disable_ipv6=1 >> log.txt
+#sysctl -w net.ipv6.conf.tun0.disable_ipv6=1 >> log.txt
 
 nordvpn set protocol tcp >> log.txt
 nordvpn set obfuscate on  >> log.txt
+nordvpn whitelist add port 22 >> log.txt
+nordvpn whitelist add port 3128 >> log.txt
 
 VPN_USERNAME=$(cat /scripts/vpn-config/username.conf)
 VPN_PASSWORD=$(cat /scripts/vpn-config/password.conf)
 VPN_COUNTRY=$(cat /scripts/vpn-config/country.conf)
 
-#nordvpn login -u ${VPN_USERNAME} -p ${VPN_PASSWORD}  >> log.txt
+nordvpn login -u ${VPN_USERNAME} -p ${VPN_PASSWORD}  >> log.txt
 
-#nordvpn connect ${VPN_COUNTRY} >> log.txt
+nordvpn connect ${VPN_COUNTRY} >> log.txt
 
 
